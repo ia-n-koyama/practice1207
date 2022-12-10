5e591a2487069f019aeba44ab0b86131917e5105
N012-SV8-7\j_okano <j_okano@internetacademy.co.jp>
2022-12-10 15:49:51 +0900

    またコメント足したよ

--- bmi-calc.js
+++ bmi-calc.js
@@ -1,5 +1,6 @@
 function calcBmi() {
     //BMIを計算します
+    //痩せるぞ！
     var height = document.getElementById("height").value
     var weight = document.getElementById("weight").value
     var bmi = weight / Math.pow(height / 100, 2)
